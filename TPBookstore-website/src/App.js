13221db8e19d540f5c40c31eb46d90dfa636dfb7
HoTai1601 <hotai1601@gmail.com>
2022-12-01 00:58:25 +0700

    tạo components và xây dựng UI home

--- TPBookstore-website/src/App.js
+++ TPBookstore-website/src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import "../src/css/App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+
+import axios from "axios";
 
 //config axios base url default
 // axios.defaults.baseURL = "https://tp-bookstore.herokuapp.com/";
 
 const App = () => {
   return (
-    <div>Hello World</div>
+    <Router>
+      <Switch>
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   );
 };
 

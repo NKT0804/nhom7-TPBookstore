ea35723bff6c3cf61bef592417ad72095aa376a5
Anh Tuan <tuandgtl7@gmail.com>
2022-12-01 00:09:35 +0700

    Cấu hình các thư mục mặc định

---
+++ TPBookstore-website/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//config axios base url default
+// axios.defaults.baseURL = "https://tp-bookstore.herokuapp.com/";
+
+const App = () => {
+  return (
+    <div>Hello World</div>
+  );
+};
+
+export default App;

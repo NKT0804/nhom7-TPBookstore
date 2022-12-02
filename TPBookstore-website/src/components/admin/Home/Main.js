3c1867bbd94431ffeedcc51d8cd3a8c72d0f69c5
Anh Tuan <tuandgtl7@gmail.com>
2022-12-02 11:45:50 +0700

    thiết kế chức năng thống kê và sơ đồ trang home-admin

---
+++ TPBookstore-website/src/components/admin/Home/Main.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import TopTotal from "./TopTotal";
+import SaleStatistics from "./SalesStatistics";
+import ProductsStatistics from "./ProductsStatistics";
+
+const Main = () => {
+
+
+  return (
+    <>
+      <section className="content-main">
+        <div className="content-header">
+          <h2 className="content-title"> Bảng điều khiển </h2>
+        </div>
+        {/* Top Total */}
+        <TopTotal />
+
+        <div className="row">
+          {/* STATICS */}
+          <SaleStatistics />
+          <ProductsStatistics />
+        </div>
+
+        {/* LATEST ORDER */}
+        <div className="card mb-4 shadow-sm">
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Main;
